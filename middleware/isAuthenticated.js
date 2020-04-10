3abd442264b32bd26568532b09d9a2d707698c35
Jamal Pelpuo <jpelpuo@ymail.com>
2020-04-10 02:52:04 +0000

    Adds authenticatio middleware

---
+++ middleware/isAuthenticated.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+module.exports = (request, response, next) => {
+    const authHeader = request.get('Authorization');
+
+    if(!authHeader){
+        request.isAuthenticated = false;
+        return next();
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if(!token || token == ''){
+        request.isAuthenticated = false;
+        return next();
+    }
+
+    let decodedToken;
+
+    try{
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    }catch(error){
+        request.isAuthenticated = false;
+        return next();
+    }
+
+    if(!decodedToken){
+        request.isAuthenticated = false;
+        return next();
+    }
+
+    request.isAuthenticated = true;
+    request.userId = decodedToken.userId;
+    return next();
+}
---
+++ utils/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+
+module.exports = connectToDatabase = async () => {
+    try{
+        const connection = await mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        if(!connection){
+            throw error;
+        }
+
+        console.log(chalk.greenBright("Connected to database"))
+    }catch(error){
+        throw error;
+    }
+}
