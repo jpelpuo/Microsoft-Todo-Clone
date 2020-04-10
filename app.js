60fba57d1fb443ef36750d4e76a6a87a4dafc937
Jamal Pelpuo <jpelpuo@ymail.com>
2020-04-10 02:48:54 +0000

    Refactors code

--- app.js
+++ app.js
@@ -2,23 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
 const Schema = require('./graphql/schema/index');
-const resolvers = require('./graphql/resolvers/index')
+const resolvers = require('./graphql/resolvers/index');
+const connectToDatabase = require('./utils/database');
+const chalk = require('chalk');
+const ENV = require('dotenv');
+const isAuthenticated = require('./middleware/isAuthenticated')
+
+ENV.config();
 
 const app = express();
 
+(async () => {
+    await connectToDatabase()
+        .catch(error => {
+            console.log(chalk.redBright("Could not connect to database"))
+        });
+})();
+
 // Body Parser Middleware
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(isAuthenticated)
+
 // Graphql middleware
 app.use('/graphql', graphqlHTTP({
-    schema : Schema,
-    rootValue: {
-        resolvers
-    },
+    schema: Schema,
+    rootValue: resolvers,
     graphiql: true
 }))
 
